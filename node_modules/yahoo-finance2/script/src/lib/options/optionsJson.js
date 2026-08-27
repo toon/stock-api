ac36704b4d06380463a8f801acd1276fc508d173
Igor Thiago Marques Mendonça <igor.mendonca@gmail.com>
2026-08-27 15:42:20 -0300

    chore: remove node_modules do versionamento

--- node_modules/yahoo-finance2/script/src/lib/options/optionsJson.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/*
- * The "JSON" part of options that can be validated against a JSON schema,
- * i.e. no functions or classes.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
